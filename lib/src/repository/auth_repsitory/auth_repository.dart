76420372ea366e1445f04003d766d2a38473e012
Lewaashaaban <lewaa03346678@gmail.com>
2023-08-23 15:14:49 +0300

    git commit

--- lib/src/repository/auth_repsitory/auth_repository.dart
+++ lib/src/repository/auth_repsitory/auth_repository.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:my/src/Pages/chat/chatScreen.dart';
