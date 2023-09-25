8bd1beb965c38013d28c14e29442964e56f04c3a
Lewaashaaban <lewaa03346678@gmail.com>
2023-09-25 17:15:59 +0300

    Add project files

---
+++ lib/src/repository/chat-repository/chat-repository.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: file_names, avoid_print
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<String> generateResponse(String prompt) async {
+  String? apiKey = "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";
+
+  var url = Uri.https("api.openai.com", "/v1/completions");
+  final response = await http.post(
+    url,
+    headers: {
+      'Content-Type': 'application/json',
+      "Authorization": "Bearer $apiKey"
+    },
+    body: json.encode({
+      "model": "text-davinci-003",
+      "prompt": prompt,
+      'temperature': 0.5,
+      'max_tokens': 2000,
+      'top_p': 1,
+      'frequency_penalty': 0.0,
+      'presence_penalty': 0.0,
+    }),
+  );
+
+  // Print the response for debugging
+  print("API Response: ${response.body}");
+
+  // Do something with the response
+  Map<String, dynamic> newResponse = jsonDecode(response.body);
+
+  // Handle the response or return it
+  if (newResponse.containsKey('choices')) {
+    return newResponse['choices'][0]['text'];
+  } else {
+    // Handle the case where 'choices' is not present in the response
+    return "Response structure does not match expectations.";
+  }
+}
